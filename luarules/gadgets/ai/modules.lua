1f9956e34e148396dbe1ed74a435494a9f45904e
ForbodingAngel <forbodingangel@gmail.com>
2016-05-01 19:09:08 -0700

    Remove base shard files

--- luarules/gadgets/ai/modules.lua
+++
@@ -1,6 +0,0 @@
-shard_include( "spothandler" )
-shard_include( "unithandler" )
-shard_include( "attackhandler" )
-shard_include( "sleep" )
-
-modules = { UnitHandler, AttackHandler, MetalSpotHandler, Sleep }
--- luarules/gadgets/ai/preload/module.lua
+++
@@ -1,9 +0,0 @@
-Module = class(AIBase)
-
-function Module:Name()
-	return "no name defined"
-end
-
-function Module:internalName()
-	return "module"
-end
